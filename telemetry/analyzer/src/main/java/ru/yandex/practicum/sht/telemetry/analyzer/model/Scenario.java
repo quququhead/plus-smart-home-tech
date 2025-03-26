955fdcfe4ccf22009ad182da7b4b4300fbd54eb9
quhead <quququhead@gmail.com>
2025-03-26 14:07:38 +0500

    fix lazily initialize a collection of model "Scenario" (conditions and actions)

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/sht/telemetry/analyzer/model/Scenario.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/sht/telemetry/analyzer/model/Scenario.java
@@ -25,12 +25,12 @@ public class Scenario {
 
     String name;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @MapKeyColumn(table = "scenario_conditions", name = "sensor_id")
     @JoinTable(name = "scenario_conditions", joinColumns = @JoinColumn(name = "scenario_id"), inverseJoinColumns = @JoinColumn(name = "condition_id"))
     private Map<String, Condition> conditions = new HashMap<>();
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @MapKeyColumn(table = "scenario_actions", name = "sensor_id")
     @JoinTable(name = "scenario_actions", joinColumns = @JoinColumn(name = "scenario_id"), inverseJoinColumns = @JoinColumn(name = "action_id"))
     private Map<String, Action> actions = new HashMap<>();
